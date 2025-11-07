be5bbb751ce6e748892095f206dd43d33ef5839e
J.J. <j.rdb08@hotmail.com>
2025-11-07 18:53:49 +0100

    ---

--- Assets/Scripts/Opdracht4/scoreManager.cs
+++ Assets/Scripts/Opdracht4/scoreManager.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
+using TMPro;
 
 public class scoreManager : MonoBehaviour
 {
-    public int score = 0; 
+    public int score = 0;
+    public TMP_Text scoreText;
+
+    void Start()
+    {
+        UpdateScoreUI();
+    }
 
     public void AddScore(int points)
     {
-        
         score += points;
+        Debug.Log("Score updated: " + score);
+        UpdateScoreUI();
+    }
 
-        
-        Debug.Log("Score bijgewerkt: " + score);
+    void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + score;
     }
 }
